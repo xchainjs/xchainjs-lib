7ab0ad49b35ef12bfc69fca5f303adc5d181f16f
Kashif <static.kash@gmail.com>
2020-09-29 16:51:57 +0900

    add interface

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,18 @@
-export const yo = 42;
+interface CoreClient {
+  phrase: string;
+  setPhrase(phrase?: string): void;
+  getBalance(address?: string): Promise<number>;
+  getTransactions(address: string): Promise<any>;
+  getExplorerUrl(): string;
+  transfer(
+    asset: string,
+    amount: number,
+    recipient: string,
+    memo: string,
+    feeRate: number
+  ): any;
+}
+
+const yo = 42;
+
+export { CoreClient, yo };
