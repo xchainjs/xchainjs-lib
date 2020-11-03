c996964c244431e1c877b5eb5742ba009bd58891
Veado <veado@protonmail.com>
2020-11-03 10:38:43 +0100

    Remove packages/*/lib
    + ignore them

--- packages/xchain-cosmos/lib/types.d.ts
+++
@@ -1,4 +0,0 @@
-import { Asset } from '@xchainjs/xchain-util';
-export declare const CosmosChain = "THOR";
-export declare const AssetAtom: Asset;
-export declare const AssetMuon: Asset;
--- packages/xchain-thorchain/lib/util.d.ts
+++
@@ -1,9 +0,0 @@
-import { Asset } from '@xchainjs/xchain-util';
-/**
- * Get denom from Asset
- */
-export declare const getDenom: (v: Asset) => string;
-/**
- * Get Asset from denom
- */
-export declare const getAsset: (v: string) => Asset | null;
