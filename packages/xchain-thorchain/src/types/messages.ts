8bbd5483e08a5893dd5498c97626f328675f5a91
junkai121 <junkai121@outlook.com>
2021-01-13 01:23:16 +1000

    update comments

--- packages/xchain-thorchain/src/types/messages.ts
+++ packages/xchain-thorchain/src/types/messages.ts
@@ -25,8 +25,10 @@ export class MsgNativeTx extends Msg {
 }
 
 /**
- *
+ * This creates MsgNativeTx from json.
+ * 
  * @param value
+ * @returns {MsgNativeTx}
  */
 export const msgNativeTxFromJson = (value: { coins: MsgCoin[]; memo: string; signer: string }): MsgNativeTx => {
   return new MsgNativeTx(value.coins, value.memo, AccAddress.fromBech32(value.signer))
