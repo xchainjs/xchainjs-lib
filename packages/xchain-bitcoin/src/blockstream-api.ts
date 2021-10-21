8fe922499f18216614fef9bf207444009829f0e3
Jonny Burger <jonathanburger11@gmail.com>
2021-10-21 11:42:10 +0200

    Update blockstream-api.ts

--- packages/xchain-bitcoin/src/blockstream-api.ts
+++ packages/xchain-bitcoin/src/blockstream-api.ts
@@ -13,8 +13,18 @@ export const broadcastTx = async ({ network, txHex, blockstreamUrl }: BroadcastT
   try {
     if (network === 'mainnet') {
       const url = 'https://api.blockcypher.com/v1/btc/main/txs/push?token=595c24fa03fa44ca8221fb67839d16f4'
-      const response = await axios.post(url, JSON.stringify({ tx: txHex }))
-      return response.data.hash
+      const response = await fetch(url, {
+        method: 'post',
+        body: JSON.stringify({ tx: txHex }),
+        headers: {
+          'content-type': 'application/json',
+        },
+      })
+      const json = await response.json()
+      if (response.status !== 201) {
+        throw new Error(JSON.stringify(json))
+      }
+      return json.tx.hash
     }
     const url = network === 'testnet' ? `${blockstreamUrl}/testnet/api/tx` : `${blockstreamUrl}/api/tx`
     const txid: string = (await axios.post(url, txHex)).data
