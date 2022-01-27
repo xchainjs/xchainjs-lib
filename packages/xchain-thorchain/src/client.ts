255289161322595a540272bc7659c30a1a621910
DollyVolley <oliver.schwarz@block42.tech>
2022-01-27 09:40:34 +0100

    Fix synth notation

--- packages/xchain-thorchain/src/client.ts
+++ packages/xchain-thorchain/src/client.ts
@@ -18,7 +18,7 @@ import {
 } from '@xchainjs/xchain-client'
 import { CosmosSDKClient, RPCTxResult, TxOfflineParams } from '@xchainjs/xchain-cosmos'
 import * as xchainCrypto from '@xchainjs/xchain-crypto'
-import { Asset, AssetRuneNative, assetFromString, baseAmount } from '@xchainjs/xchain-util'
+import { Asset, AssetRuneNative, assetFromString, assetToString, baseAmount } from '@xchainjs/xchain-util'
 import axios from 'axios'
 import { AccAddress, PrivKey, PubKey, codec } from 'cosmos-client'
 import { StdTx } from 'cosmos-client/x/auth'
@@ -445,7 +445,7 @@ class Client implements ThorchainClient, XChainClient {
     const msgNativeTx = msgNativeTxFromJson({
       coins: [
         {
-          asset: getDenomWithChain(asset),
+          asset: assetToString(asset) === assetToString(AssetRuneNative)? getDenomWithChain(asset): getDenom(asset),
           amount: amount.amount().toString(),
         },
       ],
@@ -483,7 +483,7 @@ class Client implements ThorchainClient, XChainClient {
       from: this.getAddress(walletIndex),
       to: recipient,
       amount: amount.amount().toString(),
-      asset: getDenom(asset),
+      asset: assetToString(asset) === assetToString(AssetRuneNative)? getDenomWithChain(asset): getDenom(asset),
       memo,
       fee: {
         amount: [],
@@ -531,7 +531,7 @@ class Client implements ThorchainClient, XChainClient {
       from_sequence,
       to: recipient,
       amount: amount.amount().toString(),
-      asset: getDenom(asset),
+      asset: assetToString(asset) === assetToString(AssetRuneNative)? getDenomWithChain(asset): getDenom(asset),
       memo,
       fee: {
         amount: [],
