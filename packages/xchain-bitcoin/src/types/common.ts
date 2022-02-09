374f324dc4f3f734e56c2892fe1a1b72bd547e2b
thorswapdev <thorswap.finance@gmail.com>
2022-02-09 03:55:07 -0500

    feat: fetch txHex utxos

--- packages/xchain-bitcoin/src/types/common.ts
+++ packages/xchain-bitcoin/src/types/common.ts
@@ -9,7 +9,7 @@ export type UTXO = {
   index: number
   value: number
   witnessUtxo: Witness
-  txHex: string
+  txHex?: string
 }
 
 export type BroadcastTxParams = { network: Network; txHex: string; blockstreamUrl: string }
