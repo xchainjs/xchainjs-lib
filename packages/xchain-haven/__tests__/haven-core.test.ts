0734c3504dfe061122c86eaeb28391b0f5cdb0a1
Marty <51510876+MartyHav@users.noreply.github.com>
2022-05-02 12:14:22 +0200

    add tests

--- packages/xchain-haven/__tests__/haven-core.test.ts
+++ packages/xchain-haven/__tests__/haven-core.test.ts
@@ -4,41 +4,53 @@ import { SerializedTransaction } from 'haven-core-js'
 import { HavenCoreClient } from '../src/haven/haven-core-client'
 import { HavenBalance, NetTypes } from '../src/haven/types'
 
-const mnenomonic = `haggled rekindle pimple pebbles dozen zippers perfect olympics
-popular frying goblet gemstone snug dads factual loyal
-balding annoyed lumber vary welders navy laboratory maverick olympics`
-
-//const secretViewkey = '34975f014cde57cf52f6aec3e91aab35af427f4a8a2002acebe7b45e634d7304'
-// const publicViewkey = '770a38f508ae11fd50d74cc00aa3eeb947c4eca3f5d7b4664100e76847977d95'
-
-const address = 'hvta6D5QfukiUdeidKdRw4AQ9Ddvt4o9e5jPg2CzkGhdeQGkZkU4RKDW7hajbbBLwsURMLu3S3DH6d5c8QYVYYSA6jy6XRzfPv'
-
 const client = new HavenCoreClient()
 
 describe('Haven Core Client Tests', () => {
   let balance: HavenBalance
 
-  it('init haven core client', async () => {
+  const mnenomonic = `haggled rekindle pimple pebbles dozen zippers perfect olympics
+popular frying goblet gemstone snug dads factual loyal
+balding annoyed lumber vary welders navy laboratory maverick olympics`
+
+  //const secretViewkey = '34975f014cde57cf52f6aec3e91aab35af427f4a8a2002acebe7b45e634d7304'
+  // const publicViewkey = '770a38f508ae11fd50d74cc00aa3eeb947c4eca3f5d7b4664100e76847977d95'
+
+  const address = 'hvta6D5QfukiUdeidKdRw4AQ9Ddvt4o9e5jPg2CzkGhdeQGkZkU4RKDW7hajbbBLwsURMLu3S3DH6d5c8QYVYYSA6jy6XRzfPv'
+
+  it('should init haven core client without error', async () => {
     const response = await client.init(mnenomonic, NetTypes.testnet)
     expect(response).toBeTruthy()
   })
 
-  it('get correct address', async () => {
+  it('should return correct address', async () => {
     const response = await client.getAddress()
     expect(response).toBe(address)
   })
 
-  it('get balance', async () => {
+  it('should validate address', async () => {
+    const isValid = await client.validateAddress(
+      'hvta6D5QfukiUdeidKdRw4AQ9Ddvt4o9e5jPg2CzkGhdeQGkZkU4RKDW7hajbbBLwsURMLu3S3DH6d5c8QYVYYSA6jy6XRzfPv',
+    )
+    expect(isValid).toBeTruthy()
+  })
+  it('should invalidate address', async () => {
+    const isValid = await client.validateAddress('drlgjrnvkrsuvusekhvusehvuhksuevksekuvhseuvhskedueahdkeaudhakuedhk')
+    console.log(isValid)
+    expect(isValid).toBeFalsy()
+  })
+
+  it('should return balance', async () => {
     balance = await client.getBalance()
     expect(new BigNumber(balance.XUSD.balance).isGreaterThan(0))
   })
 
-  it('get transaction', async () => {
+  it('should return transaction history', async () => {
     const transactions: SerializedTransaction[] = await client.getTransactions()
     expect(transactions.length).toBeGreaterThan(0)
   })
 
-  it('transfers funds', async () => {
+  xit('should transfer funds if unlocked funds available', async () => {
     // is equal to 0.1 XHV
     const transferAmount = new BigNumber(10).exponentiatedBy(12).dividedBy(100)
 
--- packages/xchain-haven/__tests__/openhaven-api.test.ts
+++ packages/xchain-haven/__tests__/openhaven-api.test.ts
@@ -1,17 +1,17 @@
 import * as openhaven from '../src/haven/api'
 
-const TestNetApiUrl = 'http://142.93.249.35:1984'
-
-// const mnenomonic = `haggled rekindle pimple pebbles dozen zippers perfect olympics
-// popular frying goblet gemstone snug dads factual loyal
-// balding annoyed lumber vary welders navy laboratory maverick olympics`
-
-const secretViewkey = '34975f014cde57cf52f6aec3e91aab35af427f4a8a2002acebe7b45e634d7304'
-// const publicViewkey = '770a38f508ae11fd50d74cc00aa3eeb947c4eca3f5d7b4664100e76847977d95'
-
-const address = 'hvta6D5QfukiUdeidKdRw4AQ9Ddvt4o9e5jPg2CzkGhdeQGkZkU4RKDW7hajbbBLwsURMLu3S3DH6d5c8QYVYYSA6jy6XRzfPv'
-
 xdescribe('Openhaven API Test', () => {
+  const TestNetApiUrl = 'http://142.93.249.35:1984'
+
+  // const mnenomonic = `haggled rekindle pimple pebbles dozen zippers perfect olympics
+  // popular frying goblet gemstone snug dads factual loyal
+  // balding annoyed lumber vary welders navy laboratory maverick olympics`
+
+  const secretViewkey = '34975f014cde57cf52f6aec3e91aab35af427f4a8a2002acebe7b45e634d7304'
+  // const publicViewkey = '770a38f508ae11fd50d74cc00aa3eeb947c4eca3f5d7b4664100e76847977d95'
+
+  const address = 'hvta6D5QfukiUdeidKdRw4AQ9Ddvt4o9e5jPg2CzkGhdeQGkZkU4RKDW7hajbbBLwsURMLu3S3DH6d5c8QYVYYSA6jy6XRzfPv'
+
   beforeAll(() => openhaven.setAPI_URL(TestNetApiUrl))
 
   it('returns version', async () => {
