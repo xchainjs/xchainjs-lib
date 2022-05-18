1c632dca938f96c0a2ceddbe076ebef5e7722f62
Marty <51510876+MartyHav@users.noreply.github.com>
2022-05-18 19:08:14 +0200

    Update openhaven-api.test.ts

--- packages/xchain-haven/__tests__/openhaven-api.test.ts
+++ packages/xchain-haven/__tests__/openhaven-api.test.ts
@@ -7,15 +7,17 @@ xdescribe('Openhaven API Test', () => {
   // popular frying goblet gemstone snug dads factual loyal
   // balding annoyed lumber vary welders navy laboratory maverick olympics`
 
-  const secretViewkey = '34975f014cde57cf52f6aec3e91aab35af427f4a8a2002acebe7b45e634d7304'
+  const secretViewkey = '41198d176ecedf97d48481be2723e426991eece4dd136b5cdd26645aed3ac504'
   // const publicViewkey = '770a38f508ae11fd50d74cc00aa3eeb947c4eca3f5d7b4664100e76847977d95'
 
-  const address = 'hvta6D5QfukiUdeidKdRw4AQ9Ddvt4o9e5jPg2CzkGhdeQGkZkU4RKDW7hajbbBLwsURMLu3S3DH6d5c8QYVYYSA6jy6XRzfPv'
+  const address = 'hvtaKeraSrv8KGdn7Vp6qsQwBZLkKVQAi5fMuVynVe8HE9h7B8gdjQpMeGC1QHm4G25TBNcaXHfzSbe4G8uGTF6b5FoNTbnY5z'
   let txHash: string | undefined
 
-  beforeAll(() => {
+  beforeAll(async () => {
     openhaven.setAPI_URL(TestNetApiUrl)
     openhaven.setCredentials(address, secretViewkey)
+    openhaven.login(false)
+    // done()
   })
 
   it('returns version', async () => {
@@ -26,10 +28,10 @@ xdescribe('Openhaven API Test', () => {
     expect(version.per_byte_fee).toBeGreaterThan(0)
   })
 
-  it('login/create account', async () => {
-    const loginResponse = await openhaven.login(false)
-    expect(loginResponse.status).toBe('success')
-  })
+  // it('login/create account', async () => {
+  //   const loginResponse = await openhaven.login(false)
+  //   expect(loginResponse.status).toBe('success')
+  // })
 
   it('get address info', async () => {
     const addressInnfo = await openhaven.getAddressInfo()
@@ -45,13 +47,11 @@ xdescribe('Openhaven API Test', () => {
   it('get tx info', async () => {
     expect(txHash).toBeDefined()
     const txInfo = await openhaven.getTx(txHash!)
-    console.log(txInfo)
     expect(txInfo.status).toBe('success')
   })
 
   it('keep search thread alive', async () => {
     const pingResponse = await openhaven.keepAlive()
-    console.log(pingResponse)
     expect(pingResponse.status).toBe('success')
   })
 })
