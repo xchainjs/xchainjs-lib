f527bc214bb51fc47b3619feabeb2fc49424fff0
Marty <51510876+MartyHav@users.noreply.github.com>
2022-05-18 19:08:19 +0200

    Update haven-core.test.ts

--- packages/xchain-haven/__tests__/haven-core.test.ts
+++ packages/xchain-haven/__tests__/haven-core.test.ts
@@ -1,25 +1,26 @@
 import BigNumber from 'bignumber.js'
 import { SerializedTransaction } from 'haven-core-js'
 
+import mockopenhaven from '../__mocks__/open-haven'
 import { HavenCoreClient } from '../src/haven/haven-core-client'
 import { HavenBalance, NetTypes, SyncObserver, SyncStats } from '../src/haven/types'
 
 const client = new HavenCoreClient()
 
-describe('Haven Core Client Tests', () => {
+xdescribe('Haven Core Client Tests', () => {
   let balance: HavenBalance
 
-  const mnenomonic = `haggled rekindle pimple pebbles dozen zippers perfect olympics
-popular frying goblet gemstone snug dads factual loyal
-balding annoyed lumber vary welders navy laboratory maverick olympics`
+  const mnenomonic =
+    'juvenile kickoff king glass scoop lair iris token truth puzzled amaze corrode justice autumn pimple turnip cafe oyster hover baffles giddy farming vector western pimple'
 
   //const secretViewkey = '34975f014cde57cf52f6aec3e91aab35af427f4a8a2002acebe7b45e634d7304'
   // const publicViewkey = '770a38f508ae11fd50d74cc00aa3eeb947c4eca3f5d7b4664100e76847977d95'
 
-  const address = 'hvta6D5QfukiUdeidKdRw4AQ9Ddvt4o9e5jPg2CzkGhdeQGkZkU4RKDW7hajbbBLwsURMLu3S3DH6d5c8QYVYYSA6jy6XRzfPv'
+  const address = 'hvtaKeraSrv8KGdn7Vp6qsQwBZLkKVQAi5fMuVynVe8HE9h7B8gdjQpMeGC1QHm4G25TBNcaXHfzSbe4G8uGTF6b5FoNTbnY5z'
 
   beforeAll(async () => {
     await client.preloadModule()
+    mockopenhaven.init()
   })
 
   it('should init haven core client without error', async () => {
@@ -32,15 +33,14 @@ balding annoyed lumber vary welders navy laboratory maverick olympics`
     expect(response).toBe(address)
   })
 
-  it('should validate address', async () => {
-    const isValid = await client.validateAddress(
-      'hvta6D5QfukiUdeidKdRw4AQ9Ddvt4o9e5jPg2CzkGhdeQGkZkU4RKDW7hajbbBLwsURMLu3S3DH6d5c8QYVYYSA6jy6XRzfPv',
+  it('should validate address', () => {
+    const isValid = client.validateAddress(
+      'hvtaKeraSrv8KGdn7Vp6qsQwBZLkKVQAi5fMuVynVe8HE9h7B8gdjQpMeGC1QHm4G25TBNcaXHfzSbe4G8uGTF6b5FoNTbnY5z',
     )
     expect(isValid).toBeTruthy()
   })
-  it('should invalidate address', async () => {
-    const isValid = await client.validateAddress('drlgjrnvkrsuvusekhvusehvuhksuevksekuvhseuvhskedueahdkeaudhakuedhk')
-    console.log(isValid)
+  it('should invalidate address', () => {
+    const isValid = client.validateAddress('drlgjrnvkrsuvusekhvusehvuhksuevksekuvhseuvhskedueahdkeaudhakuedhk')
     expect(isValid).toBeFalsy()
   })
 
@@ -54,15 +54,18 @@ balding annoyed lumber vary welders navy laboratory maverick olympics`
     expect(transactions.length).toBeGreaterThan(0)
   })
 
-  xit('should transfer funds if unlocked funds available', async () => {
+  xit('should transfer funds', async () => {
     // is equal to 0.1 XHV
     const transferAmount = new BigNumber(10).exponentiatedBy(12).dividedBy(100)
-
-    const hasEnoughBalance = new BigNumber(balance.XHV.unlockedBalance).isGreaterThan(transferAmount)
-    expect(hasEnoughBalance).toBeTruthy()
-    if (!hasEnoughBalance) return
     const response = await client.transfer(transferAmount.toString(), 'XHV', address)
-    console.log(response)
+    expect(typeof response).toBe('string')
+    expect(response).not.toBe('')
+  })
+
+  it('should transfer funds with memo', async () => {
+    // is equal to 0.1 XHV
+    const transferAmount = new BigNumber(10).exponentiatedBy(12).dividedBy(100)
+    const response = await client.transfer(transferAmount.toString(), 'XHV', address, 'SWAP:THOR.RUNE')
     expect(typeof response).toBe('string')
     expect(response).not.toBe('')
   })
