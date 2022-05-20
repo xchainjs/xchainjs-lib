a1a4ca572601560a5576cb582a91e1c87769cd62
GrassRoots Crypto <chris@grassrootscrypto.io>
2022-05-20 17:20:50 +0930

    added to chain utils from AsgardEx, useful for halt checking

--- packages/xchain-swap/src/utils/chain-utils.ts
+++ packages/xchain-swap/src/utils/chain-utils.ts
@@ -23,12 +23,7 @@ import {
   isChain,
 } from '@xchainjs/xchain-util'
 
-//import { eqChain } from './fp/eq'
-
-//...\asgardex-electron\src\renderer\services\const.ts
-/** const of the enbaled chains -- not sure how to get this to work */
-import { ENABLED_CHAINS } from '../services/const'
-
+// not using this atm
 export const ENABLED_CHAINS: Chain[] = envOrDefault(
   process.env.REACT_APP_CHAINS_ENABLED,
   'THOR,BNB,BTC,LTC,BCH,ETH,DOGE,TERRA',
@@ -40,6 +35,7 @@ export const ENABLED_CHAINS: Chain[] = envOrDefault(
 export const isEnabledChain = (chain: Chain) => ENABLED_CHAINS.includes(chain)
 
 /**
+ * Gets the gas asset of a chain
  *
  * @param chain
  * @returns
@@ -68,56 +64,3 @@ export const getChainAsset = (chain: Chain): Asset => {
       throw Error('Polkadot is not supported yet')
   }
 }
-
-export const eqString = S.Eq
-export const eqAsset: Eq.Eq<Asset> = {
-  equals: (x, y) => eqString.equals(assetToString(x).toUpperCase(), assetToString(y).toUpperCase()),
-}
-
-/**
- *
- * There lots of good util functions at s\thorchain\asgardex-electron\src\renderer\helpers\fp\eq.ts
- *  like eqChain, eqAsset and so on.
- *
- */
-
-// Below is taken from \thorchain\asgardex-electron\src\renderer\helpers\chainHelper.ts
-/**
- * Check whether chain is BTC chain
- */
-export const isBtcChain = (chain: Chain): boolean => eqChain.equals(chain, BTCChain)
-
-/**
- * Check whether chain is LTC chain
- */
-export const isLtcChain = (chain: Chain): boolean => eqChain.equals(chain, LTCChain)
-
-/**
- * Check whether chain is THOR chain
- */
-export const isThorChain = (chain: Chain): boolean => eqChain.equals(chain, THORChain)
-
-/**
- * Check whether chain is BNB chain
- */
-export const isBnbChain = (chain: Chain): boolean => eqChain.equals(chain, BNBChain)
-
-/**
- * Check whether chain is ETH chain
- */
-export const isEthChain = (chain: Chain): boolean => eqChain.equals(chain, ETHChain)
-
-/**
- * Check whether chain is BCH chain
- */
-export const isBchChain = (chain: Chain): boolean => eqChain.equals(chain, BCHChain)
-
-/**
- * Check whether chain is DOGE chain
- */
-export const isDogeChain = (chain: Chain): boolean => eqChain.equals(chain, DOGEChain)
-
-/**
- * Check whether chain is TERRA chain
- */
-export const isTerraChain = (chain: Chain): boolean => eqChain.equals(chain, TerraChain)
--- packages/xchain-util/src/chain.ts
+++ packages/xchain-util/src/chain.ts
@@ -30,6 +30,19 @@ export const DOGEChain = Chain.Doge
  */
 export const isChain = (c: string): c is Chain => (Object.values(Chain) as string[]).includes(c)
 
+export const isEnabledChain = (chain: Chain) => chain.includes(chain)
+
+/**
+ * Sees if one chain is equal to another chain
+ *
+ * @param a chain a
+ * @param b chain b
+ * @returns boolean: True if equal else False
+ */
+export const eqAsset = (a: Chain, b: Chain) => {
+  return a.toString == b.toString
+}
+
 /**
  * Convert chain to string.
  *
@@ -54,3 +67,43 @@ export const chainToString: ((chainId: Chain) => string) & Record<Chain, string>
     [Chain.Doge]: 'Dogecoin',
   },
 )
+
+/**
+ * Check whether chain is BTC chain
+ */
+export const isBtcChain = (chain: Chain): boolean => eqAsset(chain, BTCChain)
+
+/**
+ * Check whether chain is LTC chain
+ */
+export const isLtcChain = (chain: Chain): boolean => eqAsset(chain, LTCChain)
+
+/**
+ * Check whether chain is THOR chain
+ */
+export const isThorChain = (chain: Chain): boolean => eqAsset(chain, THORChain)
+
+/**
+ * Check whether chain is BNB chain
+ */
+export const isBnbChain = (chain: Chain): boolean => eqAsset(chain, BNBChain)
+
+/**
+ * Check whether chain is ETH chain
+ */
+export const isEthChain = (chain: Chain): boolean => eqAsset(chain, ETHChain)
+
+/**
+ * Check whether chain is BCH chain
+ */
+export const isBchChain = (chain: Chain): boolean => eqAsset(chain, BCHChain)
+
+/**
+ * Check whether chain is DOGE chain
+ */
+export const isDogeChain = (chain: Chain): boolean => eqAsset(chain, DOGEChain)
+
+/**
+ * Check whether chain is TERRA chain
+ */
+export const isTerraChain = (chain: Chain): boolean => eqAsset(chain, TerraChain)
