e80c722c524e79e14dd5d76b5541fabbbef3ae7d
Marty <51510876+MartyHav@users.noreply.github.com>
2022-05-23 19:38:59 +0200

    Update jest.config.js

--- packages/xchain-haven/jest.config.js
+++ packages/xchain-haven/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/lib'],
   transformIgnorePatterns: ['node_modules/(?!mnemonicconverter)'],
   setupFilesAfterEnv: ['./jest.setup.js'],
   silent: true,
