dacd0cf277977e6815ae1a1b022442fff14bad35
Marty <51510876+MartyHav@users.noreply.github.com>
2022-06-10 20:22:58 +0200

    redesign client to address async actions at initalization

--- packages/xchain-haven/src/types/client-types.ts
+++ packages/xchain-haven/src/types/client-types.ts
@@ -3,6 +3,6 @@ import { SyncObserver } from '../haven/types'
 export interface HavenClient {
   isSyncing(): Promise<boolean>
   subscribeSyncProgress(observer: SyncObserver): void
-  preloadSDK(): Promise<boolean>
+  unsubscribeSyncProgress(observer: SyncObserver): void
   getHavenMnemonic(): string
 }
