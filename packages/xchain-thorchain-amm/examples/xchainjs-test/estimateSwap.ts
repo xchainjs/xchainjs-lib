183e9c6ce22ba05667d5abac247e165d16c9c9eb
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2022-08-09 13:03:39 +0930

    620 fix dependency issue in xchain thorchain amm (#621)
    * finalised examples folder
    
    
    Co-authored-by: mike wyszinski <mike.wyszinski@gmail.com>

---
+++ packages/xchain-thorchain-amm/examples/xchainjs-test/estimateSwap.ts
@@ -0,0 +1,61 @@
+import { Network } from '@xchainjs/xchain-client'
+import { CryptoAmount, EstimateSwapParams, Midgard, SwapEstimate, ThorchainAMM } from '@xchainjs/xchain-thorchain-amm'
+import { assetAmount, assetFromString, assetToBase } from '@xchainjs/xchain-util'
+import BigNumber from 'bignumber.js'
+
+// Helper function for printing out the returned object
+function print(estimate: SwapEstimate, input: CryptoAmount) {
+  const expanded = {
+    input: input.formatedAssetString(),
+    totalFees: {
+      inboundFee: estimate.totalFees.inboundFee.formatedAssetString(),
+      swapFee: estimate.totalFees.swapFee.formatedAssetString(),
+      outboundFee: estimate.totalFees.outboundFee.formatedAssetString(),
+      affiliateFee: estimate.totalFees.affiliateFee.formatedAssetString(),
+    },
+    slipPercentage: estimate.slipPercentage.toFixed(),
+    netOutput: estimate.netOutput.formatedAssetString(),
+    waitTimeSeconds: estimate.waitTimeSeconds.toFixed(),
+    canSwap: estimate.canSwap,
+    errors: estimate.errors,
+  }
+  console.log(expanded)
+}
+
+/**
+ * Estimate swap function
+ * Returns estimate swap object
+ */
+const estimateSwap = async () => {
+  try {
+    const network = process.argv[2] as Network
+    const amount = process.argv[3]
+    const fromAsset = assetFromString(`${process.argv[4]}`)
+    const toAsset = assetFromString(`${process.argv[5]}`)
+    const midgard = new Midgard(network)
+    const thorchainAmm = new ThorchainAMM(midgard)
+
+    const swapParams: EstimateSwapParams = {
+      input: new CryptoAmount(assetToBase(assetAmount(amount)), fromAsset),
+      destinationAsset: toAsset,
+      // affiliateFeePercent: 0.003, //optional
+      slipLimit: new BigNumber('0.03'), //optional
+    }
+    const estimate = await thorchainAmm.estimateSwap(swapParams)
+    print(estimate, swapParams.input)
+    const estimateInFromAsset = await thorchainAmm.getFeesIn(estimate.totalFees, fromAsset)
+    estimate.totalFees = estimateInFromAsset
+    print(estimate, swapParams.input)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+// Call the function from main()
+const main = async () => {
+  await estimateSwap()
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => console.error(err))
