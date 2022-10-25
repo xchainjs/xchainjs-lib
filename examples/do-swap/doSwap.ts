e6758469974744d08aad57d0edfa65c2d80ff35e
mike wyszinski <mike.wyszinski@gmail.com>
2022-10-24 19:15:07 -0600

    updated examples

--- examples/do-swap/doSwap.ts
+++ examples/do-swap/doSwap.ts
@@ -1,5 +1,14 @@
+import { Network } from '@xchainjs/xchain-client'
 import { ThorchainAMM, Wallet } from '@xchainjs/xchain-thorchain-amm'
-import { CryptoAmount, EstimateSwapParams, ThorchainQuery, TxDetails } from '@xchainjs/xchain-thorchain-query'
+import {
+  CryptoAmount,
+  EstimateSwapParams,
+  Midgard,
+  ThorchainCache,
+  ThorchainQuery,
+  Thornode,
+  TxDetails,
+} from '@xchainjs/xchain-thorchain-query'
 import { Chain, assetAmount, assetFromString, assetToBase } from '@xchainjs/xchain-util'
 import BigNumber from 'bignumber.js'
 
@@ -31,9 +40,10 @@ function printTx(txDetails: TxDetails, input: CryptoAmount) {
  */
 const doSingleSwap = async (tcAmm: ThorchainAMM, wallet: Wallet) => {
   try {
-    const amount = process.argv[3]
-    const fromAsset = assetFromString(`${process.argv[4]}`)
-    const toAsset = assetFromString(`${process.argv[5]}`)
+    const amount = process.argv[4]
+    const decimals = Number(process.argv[5])
+    const fromAsset = assetFromString(`${process.argv[6]}`)
+    const toAsset = assetFromString(`${process.argv[7]}`)
 
     // const fromChain = fromAsset.synth ? Chain.THORChain : fromAsset.chain
     const toChain = toAsset.synth ? Chain.THORChain : toAsset.chain
@@ -44,7 +54,7 @@ const doSingleSwap = async (tcAmm: ThorchainAMM, wallet: Wallet) => {
     // console.log(await wallet.clients[fromChain].getBalance(fromAddress))
 
     const swapParams: EstimateSwapParams = {
-      input: new CryptoAmount(assetToBase(assetAmount(amount)), fromAsset),
+      input: new CryptoAmount(assetToBase(assetAmount(amount, decimals)), fromAsset),
       destinationAsset: toAsset,
       destinationAddress,
       slipLimit: new BigNumber('0.03'), //optional
@@ -64,9 +74,12 @@ const doSingleSwap = async (tcAmm: ThorchainAMM, wallet: Wallet) => {
 
 const main = async () => {
   const seed = process.argv[2]
-  const thorchainQuery = new ThorchainQuery()
+  const network = process.argv[3] as Network
+  const thorchainCache = new ThorchainCache(new Midgard(network), new Thornode(network))
+  const thorchainQuery = new ThorchainQuery(thorchainCache)
   const thorchainAmm = new ThorchainAMM(thorchainQuery)
   const wallet = new Wallet(seed, thorchainQuery)
+
   await doSingleSwap(thorchainAmm, wallet)
 }
 
