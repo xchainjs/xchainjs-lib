896820d2e5bcc36006c027a85b5707c3f6fb832c
0xp3gasus <0xp3gasus@proton.me>
2023-11-11 16:21:58 +0100

    BTC client extends from xchain-utxo

--- packages/xchain-bitcoin/src/client.ts
+++ packages/xchain-bitcoin/src/client.ts
@@ -6,13 +6,11 @@ import {
   PreparedTx,
   TxHash,
   TxParams,
-  UTXO,
-  UTXOClient,
-  UtxoClientParams,
   checkFeeBounds,
 } from '@xchainjs/xchain-client'
 import { getSeed } from '@xchainjs/xchain-crypto'
 import { Address } from '@xchainjs/xchain-util'
+import { Client as UTXOClient, UTXO, UtxoClientParams } from '@xchainjs/xchain-utxo'
 import axios from 'axios'
 import * as Bitcoin from 'bitcoinjs-lib'
 import accumulative from 'coinselect/accumulative'
