a43992a41fc12484dbb6e91f1b056a077ba59233
0xp3gasus <0xp3gasus@proton.me>
2023-11-20 23:30:01 +0100

    LCH tests

--- packages/xchain-litecoin/src/const.ts
+++ packages/xchain-litecoin/src/const.ts
@@ -14,7 +14,7 @@ import {
  * @see https://github.com/bitcoin/bitcoin/blob/db88db47278d2e7208c50d16ab10cb355067d071/src/validation.h#L56
  */
 export const MIN_TX_FEE = 1000
-export const LOWER_FEE_BOUND = 1
+export const LOWER_FEE_BOUND = 0.5
 export const UPPER_FEE_BOUND = 500
 export const LTC_DECIMAL = 8
 
