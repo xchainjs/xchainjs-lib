41535bf1db702ca1f48200e52fe33fccd44a60ec
0xp3gasus <0xp3gasus@proton.me>
2023-11-20 23:25:36 +0100

    BTC Upper fee amount duplicated

--- packages/xchain-bitcoin/src/const.ts
+++ packages/xchain-bitcoin/src/const.ts
@@ -20,7 +20,7 @@ export const MIN_TX_FEE = 1000
 export const BTC_DECIMAL = 8
 
 export const LOWER_FEE_BOUND = 1
-export const UPPER_FEE_BOUND = 500
+export const UPPER_FEE_BOUND = 1_000
 export const BTC_SYMBOL = '₿'
 export const BTC_SATOSHI_SYMBOL = '⚡'
 
