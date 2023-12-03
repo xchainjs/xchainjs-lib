05bfbae94a70b4890b383b1fbd1de640e470893e
hippocampus-web3 <hippocampus.web3@proton.me>
2023-12-03 13:37:08 +0100

    Fix native dependencies problem

--- packages/xchain-bitcoin/src/clientLedger.ts
+++ packages/xchain-bitcoin/src/clientLedger.ts
@@ -1,7 +1,7 @@
 import AppBtc from '@ledgerhq/hw-app-btc'
 import { Transaction } from '@ledgerhq/hw-app-btc/lib/types'
-import Transport from '@ledgerhq/hw-transport'
 import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
+import TransportNodeHidSingleton from '@ledgerhq/hw-transport-node-hid-singleton'
 import { FeeOption, FeeRate, TxHash, TxParams, UTXO, UtxoClientParams } from '@xchainjs/xchain-client'
 import { Address } from '@xchainjs/xchain-util'
 import * as Bitcoin from 'bitcoinjs-lib'
@@ -35,7 +35,7 @@ class ClientLedger extends Client {
     if (this.transportType === TRANSPORT_TYPES.NODE) {
       this.transport = await TransportNodeHid.create()
     } else {
-      this.transport = await Transport.create()
+      this.transport = await TransportNodeHidSingleton.create()
     }
     this.app = new AppBtc({ transport: this.transport })
     return this.app
