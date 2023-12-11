2cdfc8734b91aafa95d97e3d9f2cd5344cd1ef28
0xp3gasus <0xp3gasus@proton.me>
2023-12-12 00:50:34 +0100

    Output crytoamounts in asset notation

--- packages/xchain-thorchain-query/src/utils/index.ts
+++ packages/xchain-thorchain-query/src/utils/index.ts
@@ -1,4 +1,4 @@
-export { calcNetworkFee } from './utils'
+export { calcNetworkFee, getCryptoAmountWithNotation } from './utils'
 export { getLiquidityProtectionData, getSlipOnLiquidity, getPoolShare, getLiquidityUnits } from './liquidity'
 export * from './thornode'
 export * from './const'
