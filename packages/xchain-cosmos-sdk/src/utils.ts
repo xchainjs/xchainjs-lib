2ee8f07ef5c44b009a0ee870dfc80d781601eaf5
0xp3gasus <0xp3gasus@proton.me>
2024-01-20 16:11:18 +0100

    Utils documentation

--- packages/xchain-cosmos-sdk/src/utils.ts
+++ packages/xchain-cosmos-sdk/src/utils.ts
@@ -5,6 +5,7 @@ import { base64, bech32 } from '@scure/base'
 
 /**
  * Transform string path in HdPath compatible with cosmjs
+ *
  * @example
  * makeClientPath(`44'/118'/0'/0/0`) // returns [Slip10RawIndex.hardened(44), Slip10RawIndex.hardened(118), Slip10RawIndex.hardened(0), Slip10RawIndex.normal(0), Slip10RawIndex.normal(0) ]
  * @param fullDerivationPath
@@ -26,7 +27,19 @@ export function makeClientPath(fullDerivationPath: string): HdPath {
   return out
 }
 
+/**
+ * Transform address from Bech32 to Base64
+ *
+ * @param {string} address The address to change the format
+ * @returns the address in Base64 format
+ */
 export const bech32ToBase64 = (address: string) =>
   base64.encode(Uint8Array.from(bech32.fromWords(bech32.decode(address).words)))
 
+/**
+ * Transform address from Base64 to Bech32
+ *
+ * @param {string} address The address to change the format
+ * @returns the address in Bech32 format
+ */
 export const base64ToBech32 = (address: string, prefix: string) => toBech32(prefix, base64.decode(address))
