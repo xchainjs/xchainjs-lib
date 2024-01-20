6c0f93dad9ce54f2e8b4f5a4f1acb01db79bec1c
0xp3gasus <0xp3gasus@proton.me>
2024-01-20 13:20:06 +0100

    thorchain client e2e tests

--- packages/xchain-thorchain/__e2e__/thorchain-client.e2e.ts
+++ packages/xchain-thorchain/__e2e__/thorchain-client.e2e.ts
@@ -1,6 +1,51 @@
-import { assetToString, baseToAsset } from '@xchainjs/xchain-util'
+import { Tx } from '@xchainjs/xchain-client'
+import { assetAmount, assetToBase, assetToString, baseToAsset } from '@xchainjs/xchain-util'
 
-import { Client } from '..'
+import { AssetRuneNative as AssetRune, Client, DepositTx } from '../lib'
+
+const getPrintableTx = (tx: Tx) => {
+  return {
+    hash: tx.hash,
+    date: tx.date.toDateString(),
+    asset: assetToString(tx.asset),
+    type: tx.type,
+    from: tx.from.map((sender) => {
+      return {
+        from: sender.from,
+        asset: sender.asset ? assetToString(sender.asset) : 'undefined',
+        amount: baseToAsset(sender.amount).amount().toString(),
+      }
+    }),
+    to: tx.to.map((recipient) => {
+      return {
+        to: recipient.to,
+        asset: recipient.asset ? assetToString(recipient.asset) : 'undefined',
+        amount: baseToAsset(recipient.amount).amount().toString(),
+      }
+    }),
+  }
+}
+const getPrintableDepositTx = (depositTx: DepositTx) => {
+  return {
+    hash: depositTx.hash,
+    asset: assetToString(depositTx.asset),
+    type: depositTx.type,
+    from: depositTx.from.map((sender) => {
+      return {
+        from: sender.from,
+        asset: sender.asset ? assetToString(sender.asset) : 'undefined',
+        amount: baseToAsset(sender.amount).amount().toString(),
+      }
+    }),
+    to: depositTx.to.map((recipient) => {
+      return {
+        to: recipient.to,
+        asset: recipient.asset ? assetToString(recipient.asset) : 'undefined',
+        amount: baseToAsset(recipient.amount).amount().toString(),
+      }
+    }),
+  }
+}
 
 describe('Thorchain client e2e', () => {
   let client: Client
@@ -9,6 +54,16 @@ describe('Thorchain client e2e', () => {
     client = new Client({
       phrase: process.env.PHRASE_MAINNET,
     })
+  })
+
+  it('Should get private key', async () => {
+    const privateKey = await client.getPrivateKey(0)
+    console.log(privateKey)
+  })
+
+  it('Should get public key', async () => {
+    const publicKey = await client.getPubKey(0)
+    console.log(publicKey)
   })
 
   it('Should get address', async () => {
@@ -26,5 +81,73 @@ describe('Thorchain client e2e', () => {
         }
       }),
     )
+  })
+
+  it('Should get address transaction history', async () => {
+    const history = await client.getTransactions({ address: await client.getAddressAsync() })
+    console.log({ total: history.total })
+    history.txs.forEach((tx) => {
+      console.log(getPrintableTx(tx))
+    })
+  })
+
+  it('Should get transaction', async () => {
+    const tx = await client.getTransactionData('B949762DE7679D2C2343B135FF11EBF059DBB5DCEB1045F1D9D3DED5EB648C4C')
+    console.log(getPrintableTx(tx))
+  })
+
+  it('Should get deposit transaction', async () => {
+    const tx = await client.getDepositTransaction('3F763B3F874DC5EEEA965D570A0C8DCA68915669D38A486A826B2238447E5498')
+    console.log(getPrintableDepositTx(tx))
+  })
+
+  it('Should prepare transaction', async () => {
+    const unsignedTx = await client.prepareTx({
+      sender: await client.getAddressAsync(0),
+      recipient: await client.getAddressAsync(1),
+      amount: assetToBase(assetAmount(1, 8)),
+      memo: 'test',
+    })
+    console.log(unsignedTx)
+  })
+
+  it('Should make transfer to address 0', async () => {
+    const hash = await client.transfer({
+      recipient: await client.getAddressAsync(0),
+      amount: assetToBase(assetAmount(1, 8)),
+      memo: 'test',
+    })
+    console.log(hash)
+  })
+
+  it('Should make deposit', async () => {
+    try {
+      /**
+       * MAKE SURE TO TEST THIS FUNCTION WITH YOUR ADDRESS BNB, OTHERWISE, YOU COULD LOSE FUNDS
+       */
+      const address: string = '' || 'TO_BE_DEFINED'
+      if (address === 'TO_BE_DEFINED') throw Error('Set an address to try the deposit e2e function')
+      const memo = `=:BNB.BNB:${address}`
+
+      const hash = await client.deposit({
+        walletIndex: 0,
+        amount: assetToBase(assetAmount(1, 8)),
+        asset: AssetRune,
+        memo,
+      })
+      console.log(hash)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  })
+
+  it('Should transfer offline', async () => {
+    const txRaw = await client.transferOffline({
+      walletIndex: 0,
+      recipient: await client.getAddressAsync(0),
+      amount: assetToBase(assetAmount(1, 8)),
+    })
+    console.log(txRaw)
   })
 })
