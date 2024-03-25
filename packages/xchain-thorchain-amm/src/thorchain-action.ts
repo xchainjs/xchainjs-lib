8735bdd9b474283b0f861d94e1e27d9ff732909f
0xp3gasus <0xp3gasus@proton.me>
2024-03-25 11:16:35 +0100

    Get protocol transaction URL bug fix

--- packages/xchain-thorchain-amm/src/thorchain-action.ts
+++ packages/xchain-thorchain-amm/src/thorchain-action.ts
@@ -41,7 +41,7 @@ export class ThorchainAction {
 
     return {
       hash,
-      url: await wallet.getExplorerTxUrl(assetAmount.asset.chain, hash),
+      url: await wallet.getExplorerTxUrl(THORChain, hash),
     }
   }
 
