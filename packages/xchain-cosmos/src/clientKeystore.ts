5e22a371b0e5b51a37e067402b5e9df25d33fcfc
0xp3gasus <0xp3gasus@proton.me>
2024-04-20 17:36:56 +0200

    Cosmos Ledger client

---
+++ packages/xchain-cosmos/src/clientKeystore.ts
@@ -0,0 +1,96 @@
+import { fromBase64 } from '@cosmjs/encoding'
+import { DecodedTxRaw, DirectSecp256k1HdWallet, EncodeObject, decodeTxRaw } from '@cosmjs/proto-signing'
+import { SigningStargateClient } from '@cosmjs/stargate'
+import { TxParams } from '@xchainjs/xchain-client'
+import { MsgTypes, makeClientPath } from '@xchainjs/xchain-cosmos-sdk'
+import { getSeed } from '@xchainjs/xchain-crypto'
+import { encode, toWords } from 'bech32'
+import { fromSeed } from 'bip32'
+import { createHash } from 'crypto'
+import { publicKeyCreate } from 'secp256k1'
+
+import { Client, CosmosClientParams } from './client'
+
+export class ClientKeystore extends Client {
+  constructor(params: CosmosClientParams) {
+    super(params)
+  }
+  /**
+   * Asynchronous version of getAddress method.
+   * @param {number} index Derivation path index of the address to be generated.
+   * @returns {string} A promise that resolves to the generated address.
+   */
+  async getAddressAsync(index = 0): Promise<string> {
+    return this.getAddress(index)
+  }
+
+  /**
+   * Get the address derived from the provided phrase.
+   * @param {number | undefined} walletIndex The index of the address derivation path. Default is 0.
+   * @returns {string} The user address at the specified walletIndex.
+   */
+  public getAddress(walletIndex?: number | undefined): string {
+    const seed = getSeed(this.phrase)
+    const node = fromSeed(seed)
+    const child = node.derivePath(this.getFullDerivationPath(walletIndex || 0))
+
+    if (!child.privateKey) throw new Error('child does not have a privateKey')
+
+    // TODO: Make this method async and use CosmosJS official address generation strategy
+    const pubKey = publicKeyCreate(child.privateKey)
+    const rawAddress = this.hash160(Uint8Array.from(pubKey))
+    const words = toWords(Buffer.from(rawAddress))
+    const address = encode(this.prefix, words)
+    return address
+  }
+
+  public async transfer(params: TxParams): Promise<string> {
+    const sender = await this.getAddressAsync(params.walletIndex || 0)
+
+    const { rawUnsignedTx } = await this.prepareTx({
+      sender,
+      recipient: params.recipient,
+      asset: params.asset,
+      amount: params.amount,
+      memo: params.memo,
+    })
+
+    const unsignedTx: DecodedTxRaw = decodeTxRaw(fromBase64(rawUnsignedTx))
+
+    const signer = await DirectSecp256k1HdWallet.fromMnemonic(this.phrase as string, {
+      prefix: this.prefix,
+      hdPaths: [makeClientPath(this.getFullDerivationPath(params.walletIndex || 0))],
+    })
+
+    const signingClient = await SigningStargateClient.connectWithSigner(this.clientUrls[this.network], signer, {
+      registry: this.registry,
+    })
+
+    const messages: EncodeObject[] = unsignedTx.body.messages.map((message) => {
+      return { typeUrl: this.getMsgTypeUrlByType(MsgTypes.TRANSFER), value: signingClient.registry.decode(message) }
+    })
+
+    const tx = await signingClient.signAndBroadcast(
+      sender,
+      messages,
+      this.getStandardFee(this.getAssetInfo().asset),
+      unsignedTx.body.memo,
+    )
+
+    return tx.transactionHash
+  }
+
+  /**
+   * Hashes a buffer using SHA256 followed by RIPEMD160 or RMD160.
+   * @param {Uint8Array} buffer The buffer to hash
+   * @returns {Uint8Array} The hashed buffer
+   */
+  private hash160(buffer: Uint8Array): Buffer {
+    const sha256Hash: Buffer = createHash('sha256').update(buffer).digest()
+    try {
+      return createHash('rmd160').update(sha256Hash).digest()
+    } catch (err) {
+      return createHash('ripemd160').update(sha256Hash).digest()
+    }
+  }
+}
--- packages/xchain-cosmos/src/index.ts
+++ packages/xchain-cosmos/src/index.ts
@@ -1,3 +1,4 @@
-export * from './client' // Export all from the client module
+export { ClientKeystore as Client, ClientKeystore } from './clientKeystore' // Export all from the client module
+export { ClientLedger } from './clientLedger' // Export all from the client module
 export * from './utils' // Export all from the utils module
 export * from './const' // Export all from the const module
