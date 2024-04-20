8e9f672176801d155bcf33178f1a8ea673185979
0xp3gasus <0xp3gasus@proton.me>
2024-04-20 17:35:57 +0200

    Keystore part moved to chain client

--- packages/xchain-kujira/src/client.ts
+++ packages/xchain-kujira/src/client.ts
@@ -1,11 +1,22 @@
 import { StdFee } from '@cosmjs/amino'
-import { toBase64 } from '@cosmjs/encoding'
-import { TxBodyEncodeObject } from '@cosmjs/proto-signing'
-import { GasPrice, calculateFee } from '@cosmjs/stargate'
+import { fromBase64, toBase64 } from '@cosmjs/encoding'
+import {
+  DecodedTxRaw,
+  DirectSecp256k1HdWallet,
+  EncodeObject,
+  TxBodyEncodeObject,
+  decodeTxRaw,
+} from '@cosmjs/proto-signing'
+import { GasPrice, SigningStargateClient, calculateFee } from '@cosmjs/stargate'
 import { AssetInfo, PreparedTx, TxParams } from '@xchainjs/xchain-client'
-import { Client as CosmosSdkClient, CosmosSdkClientParams, MsgTypes } from '@xchainjs/xchain-cosmos-sdk'
+import { Client as CosmosSdkClient, CosmosSdkClientParams, MsgTypes, makeClientPath } from '@xchainjs/xchain-cosmos-sdk'
+import { getSeed } from '@xchainjs/xchain-crypto'
 import { Address, Asset, eqAsset } from '@xchainjs/xchain-util'
+import { encode, toWords } from 'bech32'
+import { fromSeed } from 'bip32'
 import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx'
+import { createHash } from 'crypto'
+import { publicKeyCreate } from 'secp256k1'
 
 import { AssetKUJI, AssetUSK, KUJI_DECIMAL, MSG_SEND_TYPE_URL, USK_ASSET_DENOM, USK_DECIMAL } from './const'
 import { defaultClientConfig, getDefaultExplorers } from './utils'
@@ -25,6 +36,71 @@ export class Client extends CosmosSdkClient {
       ...defaultClientConfig,
       ...config,
     })
+  }
+
+  /**
+   * Asynchronous version of getAddress method.
+   * @param {number} index Derivation path index of the address to be generated.
+   * @returns {string} A promise that resolves to the generated address.
+   */
+  async getAddressAsync(index = 0): Promise<string> {
+    return this.getAddress(index)
+  }
+
+  /**
+   * Get the address derived from the provided phrase.
+   * @param {number | undefined} walletIndex The index of the address derivation path. Default is 0.
+   * @returns {string} The user address at the specified walletIndex.
+   */
+  public getAddress(walletIndex?: number | undefined): string {
+    const seed = getSeed(this.phrase)
+    const node = fromSeed(seed)
+    const child = node.derivePath(this.getFullDerivationPath(walletIndex || 0))
+
+    if (!child.privateKey) throw new Error('child does not have a privateKey')
+
+    // TODO: Make this method async and use CosmosJS official address generation strategy
+    const pubKey = publicKeyCreate(child.privateKey)
+    const rawAddress = this.hash160(Uint8Array.from(pubKey))
+    const words = toWords(Buffer.from(rawAddress))
+    const address = encode(this.prefix, words)
+    return address
+  }
+
+  public async transfer(params: TxParams): Promise<string> {
+    const sender = await this.getAddressAsync(params.walletIndex || 0)
+
+    const { rawUnsignedTx } = await this.prepareTx({
+      sender,
+      recipient: params.recipient,
+      asset: params.asset,
+      amount: params.amount,
+      memo: params.memo,
+    })
+
+    const unsignedTx: DecodedTxRaw = decodeTxRaw(fromBase64(rawUnsignedTx))
+
+    const signer = await DirectSecp256k1HdWallet.fromMnemonic(this.phrase as string, {
+      prefix: this.prefix,
+      hdPaths: [makeClientPath(this.getFullDerivationPath(params.walletIndex || 0))],
+    })
+
+    const signingClient = await SigningStargateClient.connectWithSigner(this.clientUrls[this.network], signer, {
+      registry: this.registry,
+    })
+
+    const messages: EncodeObject[] = unsignedTx.body.messages.map((message) => {
+      return { typeUrl: this.getMsgTypeUrlByType(MsgTypes.TRANSFER), value: signingClient.registry.decode(message) }
+    })
+
+    const tx = await signingClient.signAndBroadcast(
+      sender,
+      messages,
+      this.getStandardFee(this.getAssetInfo().asset),
+      unsignedTx.body.memo,
+    )
+
+    return tx.transactionHash
   }
 
   /**
@@ -172,5 +248,19 @@ export class Client extends CosmosSdkClient {
     const denom = this.getDenom(asset)
     const defaultGasPrice = GasPrice.fromString(`0.025${denom}`)
     return calculateFee(90_000, defaultGasPrice)
+  }
+
+  /**
+   * Hashes a buffer using SHA256 followed by RIPEMD160 or RMD160.
+   * @param {Uint8Array} buffer The buffer to hash
+   * @returns {Uint8Array} The hashed buffer
+   */
+  private hash160(buffer: Uint8Array): Buffer {
+    const sha256Hash: Buffer = createHash('sha256').update(buffer).digest()
+    try {
+      return createHash('rmd160').update(sha256Hash).digest()
+    } catch (err) {
+      return createHash('ripemd160').update(sha256Hash).digest()
+    }
   }
 }
