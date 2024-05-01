f05a1962cb1588d145c06256ba5286b715732f2e
0xp3gasus <0xp3gasus@proton.me>
2024-05-01 13:17:00 +0200

    EVM Clients with signers

--- packages/xchain-arbitrum/src/client.ts
+++ packages/xchain-arbitrum/src/client.ts
@@ -1,5 +1,6 @@
 // Import the Client class from '@xchainjs/xchain-evm' module
-import { Client as XchainEvmClient } from '@xchainjs/xchain-evm'
+import { Network } from '@xchainjs/xchain-client'
+import { Client as XchainEvmClient, EVMClientParams, KeystoreSigner } from '@xchainjs/xchain-evm'
 
 // Import defaultArbParams constant from './const' file
 import { defaultArbParams } from './const'
@@ -7,8 +8,19 @@ import { defaultArbParams } from './const'
 // Create a class called Client that extends the XchainEvmClient class
 export class Client extends XchainEvmClient {
   // Constructor function that takes an optional config parameter, defaulting to defaultArbParams
-  constructor(config = defaultArbParams) {
+  constructor(config: Omit<EVMClientParams, 'signer'> = defaultArbParams) {
     // Call the constructor of the parent class (XchainEvmClient) with the provided config
-    super(config)
+    super({
+      ...config,
+      signer: config.phrase
+        ? new KeystoreSigner({
+            phrase: config.phrase,
+            provider: config.providers[config.network || Network.Mainnet],
+            derivationPath: config.rootDerivationPaths
+              ? config.rootDerivationPaths[config.network || Network.Mainnet]
+              : '',
+          })
+        : undefined,
+    })
   }
 }
