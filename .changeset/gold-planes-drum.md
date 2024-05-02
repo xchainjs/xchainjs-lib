0fdfb1d28930fff3c3e776352c636b9c83ed1ad0
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2024-05-02 09:32:32 +0930

    Refactor Doge ledger Client (#1134)

---
+++ .changeset/gold-planes-drum.md
@@ -0,0 +1,5 @@
+---
+'@xchainjs/xchain-doge': patch
+---
+
+Used function PrepareTx instead of Buildtx
--- packages/xchain-doge/__e2e__/doge-ledger-client.e2e.ts
+++ packages/xchain-doge/__e2e__/doge-ledger-client.e2e.ts
@@ -30,7 +30,7 @@ const defaultDogeParams: UtxoClientParams = {
 
 jest.setTimeout(200000)
 
-describe('BitcoinCash Client Ledger', () => {
+describe('Doge Client Ledger', () => {
   let dogeClient: ClientLedger
   beforeAll(async () => {
     const transport = await TransportNodeHid.create()
@@ -67,6 +67,23 @@ describe('BitcoinCash Client Ledger', () => {
         recipient: to,
         amount,
         memo: 'test',
+        feeRate: 1,
+      })
+      console.log(JSON.stringify(txid, null, 2))
+    } catch (err) {
+      console.error('ERR running test', err)
+      fail()
+    }
+  })
+  it('transfer Ledger test amount long memo', async () => {
+    try {
+      const to = await dogeClient.getAddressAsync(1)
+      const amount = assetToBase(assetAmount('1'))
+      const txid = await dogeClient.transfer({
+        asset: AssetDOGE,
+        recipient: to,
+        amount,
+        memo: '=:r:thor1tqpyn3athvuj8dj7nu5fp0xm76ut86sjcl3pqu:0/1/0',
         feeRate: 1,
       })
       console.log(JSON.stringify(txid, null, 2))
