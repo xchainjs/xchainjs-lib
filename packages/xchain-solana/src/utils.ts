b4b787fd9cd69f01e5796d43cba57194ba9269ca
0xp3gasus <0xp3gasus@proton.me>
2024-09-04 18:12:06 +0200

    Comments

--- packages/xchain-solana/src/utils.ts
+++ packages/xchain-solana/src/utils.ts
@@ -2,11 +2,10 @@ import { Cluster } from '@solana/web3.js'
 import { Network } from '@xchainjs/xchain-client'
 
 export const getSolanaNetwork = (network: Network): Cluster => {
-  switch (network) {
-    case Network.Mainnet:
-    case Network.Stagenet:
-      return 'mainnet-beta'
-    case Network.Testnet:
-      return 'testnet'
+  const networkMap: { [key in Network]: Cluster } = {
+    [Network.Mainnet]: 'mainnet-beta',
+    [Network.Stagenet]: 'mainnet-beta',
+    [Network.Testnet]: 'testnet',
   }
+  return networkMap[network]
 }
