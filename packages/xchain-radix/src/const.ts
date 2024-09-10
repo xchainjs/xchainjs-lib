26d671471c4aeab0fe60756788e9e9427cff9b3c
0xp3gasus <0xp3gasus@proton.me>
2024-09-11 00:00:49 +0200

    RADIX_ASSET_ID renamed to RADIX_ASSET_RESOURCE

--- packages/xchain-radix/src/const.ts
+++ packages/xchain-radix/src/const.ts
@@ -13,17 +13,17 @@ export const STOKENET_GATEWAY_URL = 'https://stokenet.radixdlt.com'
 
 export const XRD_DECIMAL = 18
 
-export const RADIX_ASSET_ID = 'resource_tdx_2_1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxtfd2jc'
+export const RADIX_ASSET_RESOURCE = 'resource_tdx_2_1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxtfd2jc'
 
 export const XrdAssetMainnet: Asset = {
-  symbol: `XDR-${RADIX_ASSET_ID}`,
+  symbol: `XRD`,
   ticker: 'XRD',
   type: AssetType.NATIVE,
   chain: RadixChain,
 }
 
 export const XrdAssetStokenet: Asset = {
-  symbol: `XDR-${RADIX_ASSET_ID}`,
+  symbol: `XRD`,
   ticker: 'XRD',
   type: AssetType.NATIVE,
   chain: RadixChain,
@@ -58,13 +58,13 @@ export const feesEstimationPublicKeys: { [networkId: number]: FeesEstimationPara
   [NetworkId.Mainnet]: {
     from: 'account_rdx12xh48d5s9u7me5t49z25lrm4h73wclqpjvumd49ctf0ggnyazc62m8',
     to: 'account_rdx1685t40mreptjhs9g3pg9lgf7k7rgppzjeknjgrpc7d0sumcjrsw6kj',
-    resourceAddress: RADIX_ASSET_ID,
+    resourceAddress: RADIX_ASSET_RESOURCE,
     publicKey: 'a47e22f21e16d80374f16d66224b56f6eda82a6db8279de267a74a49f0291e8b',
   },
   [NetworkId.Stokenet]: {
     from: 'account_tdx_2_12927ya6vxtmhu8w0qkwtumw8kjmlv930agjzezfgg6yp3j6agn3gfc',
     to: 'account_tdx_2_12xnfu4evyseqeq57rzhrh8ls6wy76vvc4jnw2kzx3l5ka7wyddxh3l',
-    resourceAddress: RADIX_ASSET_ID,
+    resourceAddress: RADIX_ASSET_RESOURCE,
     publicKey: '3ce4d36fd8bf40fa6f9b0cf2ef8d11853d088589ebdc79055f5a0af55bf7e758',
   },
 }
--- packages/xchain-radix/src/utils.ts
+++ packages/xchain-radix/src/utils.ts
@@ -1,5 +1,8 @@
-import { Asset, TokenAsset } from '@xchainjs/xchain-util'
+import { Asset, TokenAsset, eqAsset } from '@xchainjs/xchain-util'
+
+import { RADIX_ASSET_RESOURCE, XrdAssetMainnet, XrdAssetStokenet } from './const'
 
 export const getAssetResource = (asset: Asset | TokenAsset): string => {
+  if (eqAsset(asset, XrdAssetMainnet) || eqAsset(asset, XrdAssetStokenet)) return RADIX_ASSET_RESOURCE
   return asset.symbol.slice(asset.ticker.length + 1)
 }
