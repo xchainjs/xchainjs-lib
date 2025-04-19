a1269c10d2894023c8294973b17eff9d51f29da3
hippocampus-web3 <hippocampus.web3@proton.me>
2025-04-19 20:28:20 +0200

    Optimize

--- packages/xchain-ethereum/rollup.config.js
+++ packages/xchain-ethereum/rollup.config.js
@@ -2,8 +2,14 @@ import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 import pkg from './package.json'
+
+const external = (id) => {
+  const deps = Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {}));
+  return deps.some(dep => id === dep || id.startsWith(`${dep}/`));
+};
 
 export default {
   input: 'src/index.ts',
@@ -13,13 +19,13 @@ export default {
       format: 'cjs',
       interop: 'auto',
       exports: 'named',
-      sourcemap: true,
+      sourcemap: false,
     },
     {
       file: pkg.module,
       format: 'es',
       exports: 'named',
-      sourcemap: true,
+      sourcemap: false,
     },
   ],
   plugins: [
@@ -33,6 +39,12 @@ export default {
       exclude: '**/*.json',
     }),
     json(),
+    visualizer({
+      filename: 'stats.html',
+      gzipSize: true,
+      brotliSize: true,
+      open: false,
+    })
   ],
-  external: Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
+  external
 }
