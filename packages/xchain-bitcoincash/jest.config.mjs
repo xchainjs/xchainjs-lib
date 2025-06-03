ce71dc955c8ceb464250933ce6ecda896fbb0d0a
dynst <148708712+dynst@users.noreply.github.com>
2025-06-03 00:00:00 +0000

    config.mjs filenames

--- packages/xchain-bitcoincash/jest.config.mjs
+++ packages/xchain-bitcoincash/jest.config.mjs
@@ -1,7 +1,5 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/lib'],
-  testMatch: ['<rootDir>/__e2e__/**/*.[jt]s?(x)'],
-  testTimeout: 30000,
 }
--- packages/xchain-thorchain/jest.config.mjs
+++ packages/xchain-thorchain/jest.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/lib'],
