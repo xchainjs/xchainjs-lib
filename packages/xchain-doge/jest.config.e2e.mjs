7cf31b0231149879dc77b18d52628084b4f9881b
dynst <148708712+dynst@users.noreply.github.com>
2025-06-03 00:00:00 +0000

    config.mjs filenames

--- packages/xchain-doge/jest.config.e2e.mjs
+++ packages/xchain-doge/jest.config.e2e.mjs
@@ -1,5 +1,7 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/lib'],
+  testMatch: ['<rootDir>/__e2e__/**/*.[jt]s?(x)'],
+  testTimeout: 30000,
 }
