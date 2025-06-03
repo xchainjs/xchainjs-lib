7ecd27fd2cbde4da5ea6802fa42cfcf4896aea89
dynst <148708712+dynst@users.noreply.github.com>
2025-06-03 00:00:00 +0000

    rename the rest of config.mjs

--- packages/xchain-cardano/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/lib'],
-  testTimeout: 60000,
-  moduleNameMapper: {
-    '@emurgo/cardano-serialization-lib-browser': '<rootDir>/__mocks__/@emurgo/cardano-serialization-lib-browser.js',
-  },
-}
