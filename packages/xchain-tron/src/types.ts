7d2436097983667fc5e50f0f1dc7119a272539fd
Mr.Tommy <smartommyit@gmail.com>
2025-09-23 22:37:43 -0400

    feat: add xchain-tron (#1539)
    * feat: add xchain-tron
    
    * chore: add yarn lock
    
    * chore: fix race condition in the key signer
    
    * chore: fix trx symbol

---
+++ packages/xchain-tron/src/types.ts
@@ -0,0 +1,47 @@
+import { ExplorerProviders, XChainClientParams } from '@xchainjs/xchain-client'
+import type { Address, BaseAmount } from '@xchainjs/xchain-util/lib'
+import type { Contract, Types } from 'tronweb'
+
+export type TRONClientParams = XChainClientParams & {
+  explorerProviders: ExplorerProviders
+}
+
+// Re-export TronWeb types for convenience
+export type TronTransaction = Types.Transaction
+export type TronContract = Contract
+export type TronSignedTransaction = Types.SignedTransaction
+
+// Signer interface compatible with TronWeb and wallet implementations
+export interface TronSigner {
+  getAddress(): Promise<string>
+  signTransaction(transaction: TronTransaction): Promise<TronSignedTransaction>
+}
+
+export type ApproveParams = {
+  walletIndex?: number
+  contractAddress: Address
+  spenderAddress: Address
+  amount?: BaseAmount
+}
+
+export type IsApprovedParams = {
+  walletIndex?: number
+  contractAddress: Address
+  spenderAddress: Address
+  amount?: BaseAmount
+}
+
+export type TronGetApprovedParams = {
+  contractAddress: string
+  spenderAddress: string
+  from: string
+}
+
+export interface TronGridTokenInfo {
+  symbol: string
+  address: string
+  decimals: number
+  name: string
+  totalSupply: string
+  owner: string
+}
