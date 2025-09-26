1676d929146bbada49e254443c471b111b54d02e
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2025-09-26 11:33:21 +0930

    added to default config

--- packages/xchain-aggregator/src/const.ts
+++ packages/xchain-aggregator/src/const.ts
@@ -1,7 +1,9 @@
+import { Network } from '@xchainjs/xchain-client'
 import { Config, Protocol } from './types'
 
 export const SupportedProtocols: Protocol[] = ['Thorchain', 'Mayachain', 'Chainflip']
 
 export const DEFAULT_CONFIG: Required<Omit<Config, 'wallet' | 'affiliate'>> = {
   protocols: SupportedProtocols,
+  network: Network.Mainnet,
 }
